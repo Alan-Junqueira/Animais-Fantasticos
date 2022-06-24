c292448439013920f1c54ed1576cfc1774bb598c
Alan Junqueira <contato.alanjunqueira@gmail.com>
2022-06-23 23:13:54 -0300

    anima numeros(MutationObserver), menu mobile, tooltip (map), otside click module

---
+++ js/modules/toolTip.js
@@ -0,0 +1,48 @@
+export default function InitToolTip() {
+  const tooltips = document.querySelectorAll('[data-tooltip]')
+
+  function criarTooltipBox(element) {
+    const tooltipBox = document.createElement('div')
+    const text = element.getAttribute('aria-label')
+
+    tooltipBox.classList.add('tooltip')
+    tooltipBox.innerText = text
+
+    document.body.appendChild(tooltipBox) //Adiciona a tooltip ao final da página.
+
+    return tooltipBox
+  }
+
+  const onMouseLeave = {
+    handleEvent() {
+      //tem que ser exatamente o handleEvent
+      this.tootipBox.remove()
+      this.element.removeEventListener('mouseleave', onMouseLeave)
+      this.element.removeEventListener('mousemove', onMouseMove)
+    }
+  }
+
+  const onMouseMove = {
+    handleEvent(event) {
+      this.tooltipBox.style.top = event.pageY + 20 + 'px' //Se não adicionar o px ele não funciona
+      this.tooltipBox.style.left = event.pageX + 20 + 'px' //Pois recebe um número inteiro.
+    } //Foi adicionado 20px pra aumentar o tamanho da caixa e tirar um bug.
+  }
+
+  //onMouseLeave e onMouseMove esta sendo criado como objeto, para não usar uma função dentro da outra no onMouseOver.
+
+  function onMouseOver(event) {
+    const tooltipBox = criarTooltipBox(this)
+
+    onMouseMove.tooltipBox = tooltipBox
+    this.addEventListener('mousemove', onMouseMove)
+
+    onMouseLeave.tootipBox = tooltipBox
+    onMouseLeave.element = this
+    this.addEventListener('mouseleave', onMouseLeave)
+  }
+
+  tooltips.forEach(item => {
+    item.addEventListener('mouseover', onMouseOver)
+  })
+}
---
+++ js/script.js
@@ -0,0 +1,19 @@
+import initTabNav from './modules/initTabNav.js'
+import accordion from './modules/accordion.js'
+import initAnimacaoScroll from './modules/initAnimacaoScroll.js'
+import initScrollSuave from './modules/initScrollSuave.js'
+import initModal from './modules/modal.js'
+import initToolTip from './modules/toolTip.js'
+import initDropDownMenu from './modules/dropdown-menu.js'
+import initMenuMobile from './modules/menu-mobile.js'
+import initAnimaNumeros from './modules/anima-numeros.js'
+
+initTabNav()
+accordion()
+initAnimacaoScroll()
+initScrollSuave()
+initModal()
+initToolTip()
+initDropDownMenu()
+initMenuMobile()
+initAnimaNumeros()
